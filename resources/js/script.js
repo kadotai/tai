e1c21fff38e8b154a69f6114bcd61ff3d56f5e9e
naopiiii <nmam44nao@icloud.com>
2024-11-14 09:50:52 +0800

    モーダル追加

---
+++ resources/js/script.js
@@ -0,0 +1,19 @@
+document.getElementById('openModalButton').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'block';
+    document.getElementById('modalOverlay').style.display = 'block';
+});
+
+document.getElementById('closeModalButton').addEventListener('click', function() {
+    document.getElementById('modal').style.display = 'none';
+    document.getElementById('modalOverlay').style.display = 'none';
+});
+
+document.getElementById('openModalButton1').addEventListener('click', function() {
+    document.getElementById('modal1').style.display = 'block';
+    document.getElementById('modalOverlay1').style.display = 'block';
+});
+
+document.getElementById('closeModalButton1').addEventListener('click', function() {
+    document.getElementById('modal1').style.display = 'none';
+    document.getElementById('modalOverlay1').style.display = 'none';
+});
