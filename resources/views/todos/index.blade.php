05b0e6277c232aa1cdc8a0467c4bad727a38f255
naopiiii <nmam44nao@icloud.com>
2024-11-20 11:09:55 +0800

    modal_change
    整頓しました

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -24,6 +24,10 @@
                     @else
                     <p>Please log in</p> <!-- ログインしていない場合のメッセージ -->
                 @endif
+                <form id="searchForm" action="{{ route('todos.index') }}" method="GET" style="margin-bottom: 10px;">
+                    <input type="text" id="searchInput" name="search" placeholder="Search by title or content" value="{{ request('search') }}">
+                    <button type="submit">Search</button>
+                </form>
             </div>
             <!-- 新規投稿モーダル -->
             <div id="modal">
@@ -33,13 +37,19 @@
             <input class="text1" name='title' placeholder="Title" maxlength="30">
             <br>
             <textarea class="text2" name='contents' placeholder="Contents" maxlength="140"></textarea>
+            <!-- 期日入力フォーム -->
+            <br>
+            <label for="due_date">〆</label>
+            <input type="date" name="due_date" id="due_date">
+            <br>
             <br>
             <label for="image" class="image_button">Select Image</label>
             <input id="image" type="file" name="image" accept="image/*" style="display: none;">
-            <!-- 期日入力フォーム -->
+            {{-- <!-- 期日入力フォーム -->
+            <br>
             <label for="due_date">Due Date:</label>
             <input type="date" name="due_date" id="due_date">
-            <br>
+            <br> --}}
             <button id="closeModalButton">Ok</button>
             <a class="dropdown-toggle1" href="/todos">&nbsp;Back</a>
             </form>
@@ -64,10 +74,10 @@
             @endif
 
             </div>
-            <form id="searchForm" action="{{ route('todos.index') }}" method="GET" style="margin-bottom: 10px;">
+            {{-- <form id="searchForm" action="{{ route('todos.index') }}" method="GET" style="margin-bottom: 10px;">
                 <input type="text" id="searchInput" name="search" placeholder="Search by title or content" value="{{ request('search') }}">
                 <button type="submit">Search</button>
-            </form>
+            </form> --}}
         </div>
     </header>
     <main>
